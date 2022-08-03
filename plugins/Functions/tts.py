47b8cd19f205a01b5837f55e9023bc951172777a
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2022-08-03 11:11:05 +0530

    Create tts.py

---
+++ plugins/Functions/tts.py
@@ -0,0 +1,23 @@
+from traceback import format_exc
+from pyrogram import Client, filters
+from asyncio import get_running_loop
+from Lucifer.functions import *
+
+@Client.on_message(filters.command("tts"))
+async def text_to_speech_en(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text("**Reply To Same Text FFS**")
+    if not message.reply_to_message.text:
+        return await message.reply_text("**Reply To Same Text FFS**")
+    m = await message.reply_text("__Processing...⏳️__")
+    text = message.reply_to_message.text
+    try:
+        loop = get_running_loop()
+        audio = await loop.run_in_executor(None, convert, text)
+        await message.reply_audio(audio)
+        await m.delete()
+        audio.close()
+    except Exception as e:
+        await m.edit(e)
+        e = format_exc()
+        print(e)
