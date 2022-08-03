0d138924e795f8d1e98a682826cc4262231dc563
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2022-08-03 16:05:00 +0530

    Create telegraph.py

---
+++ plugins/Functions/telegraph.py
@@ -0,0 +1,70 @@
+import os
+import time
+import math
+import json
+import string
+import random
+import traceback
+import asyncio
+import datetime
+import aiofiles
+from dotenv import load_dotenv
+from random import choice 
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid, UserNotParticipant, UserBannedInChannel
+from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
+from telegraph import upload_file
+from database import Database
+
+
+@Bot.on_message(filters.media & filters.command(["telegraph"]))
+async def telegraph_upload(bot, update):
+    
+    if not await db.is_user_exist(update.from_user.id):
+        await db.add_user(update.from_user.id)
+    
+    text = await update.reply_text(
+        text="<code>Downloading to My Server ...</code>",
+        disable_web_page_preview=True
+    )
+    media = await update.download()
+    
+    await text.edit_text(
+        text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>",
+        disable_web_page_preview=True
+    )
+    
+    try:
+        response = upload_file(media)
+    except Exception as error:
+        print(error)
+        await text.edit_text(
+            text=f"Error :- {error}",
+            disable_web_page_preview=True
+        )
+        return
+    
+    try:
+        os.remove(media)
+    except Exception as error:
+        print(error)
+        return
+    
+    await text.edit_text(
+        text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>Join :-</b> @FayasNoushad",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"),
+                    InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
+                ],
+                [InlineKeyboardButton(text="⚙ Join Updates Channel ⚙", url="https://telegram.me/FayasNoushad")]
+            ]
+        )
+    )
+
+
+ 
+Bot.run()
