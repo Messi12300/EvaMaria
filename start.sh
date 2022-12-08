26747b5ecce233df4bdd63a2b94372c8158ecc73
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2022-12-08 18:32:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,13 +11,4 @@ pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
 
-import os
-from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return 'Hello, World!'
-
-#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/Messi12300/EvaMaria ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
